2dc755d8f86e9d58867e738e9c0448f53945fc9c
ckondek <chef@loanshark.de>
2020-06-04 15:31:52 +0200

    small fixes

--- scripts/script.js
+++ scripts/script.js
@@ -7,8 +7,8 @@ var works=[
             {'opera':'BETWEEN TWO WORLDS','place':'LYRIC OPERA OF CHICAGO<br>BIELEFELD, GERMANY', 'when':'1997','composer':'Shulamit Ran'},
             {'opera':'SEASONS IN HELL','place':'CORBETT THEATER, CINCINATTI<br>Albany Records', 'when':'FEB 1996','composer':'Harold Blumenfeld'},
             {'opera':'THE FAN','place':'LYRIC OPERA OF CHICAGO', 'when':'FEB 1996','composer':'Lee Goldstein'},
-            {'opera':'GINA AND<br>THE PRINCE OF MINTZ','place':'NYS THEATER INSTITUTE, TROY, NY', 'when':'-','composer':'Steve Liebman'},
-            {'opera':'EVOL SPELLED<br>BACKWARDS IS LOVE','place':'GOETHE HAUS, DARMSTADT', 'when':'-','composer':'Hugh McGuiness'}
+            {'opera':'GINA and<br>The PRINCE of MINTZ','place':'NYS THEATER INSTITUTE, TROY, NY', 'when':'-','composer':'Steve Liebman'},
+            {'opera':'EVOL SPELLED<br>BACKWARDS IS LOVE','place':'GOETHE HAUS, DARMSTADT, GERMANY', 'when':'-','composer':'Hugh McGuiness'}
           ];
 
 var translations=[
@@ -16,7 +16,7 @@ var translations=[
             {'opera':'WIENER BLUT','place':'WASHINGTON (DC) NATIONAL OPERA', 'when':'','composer':'Johann Strauss, II'},
             {'opera':'THE DESPERATE HUSBAND', 'place':'SPOLETO FESTIVAL','when':'','composer':'Domenico Cimarosa'},
             {'opera':'BARBE-BLEU<br>THE GRAND DUCHESS','place':'GLIMMERGLASS OPERA', 'when':'','composer':'Jacques Offenbach'},
-            {'opera':'VERONIQUE','place':'OHIO LIGHT OPERA<br>Newport Classics', 'when':'','composer':'André Messanger'}
+            {'opera':'VÉRONIQUE','place':'OHIO LIGHT OPERA<br>Newport Classics', 'when':'','composer':'André Messager'}
 
           ];
 
